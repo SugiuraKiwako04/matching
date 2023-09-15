0336ddc778f871c56dc2a05493c095618af8754d
SugiuraKiwako04 <s14202101999@toyo.jp>
2023-09-15 20:36:47 +0900

    リサーチページの作成

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,28 @@
+<x-app-layout>
+    <x-slot name="header">
+        Index
+    </x-slot>
+    
+    　<div class="button">
+        <button class="button1">探す</button>
+        <button class="button2">おすすめ</button>
+      </div>
+      
+      <div class="seachBox2">
+       <div class="seachBox">
+          <p>キーワードから探す</p>
+          <a href>ボタン</a>
+       </div>
+      </div>
+      
+      <div class="seachBox2">
+       <div class="seachBox">
+          <p class="text-green-400">条件を指定して探す</p>
+          <a href>ボタン</a>
+          <a href>ボタン</a>
+       </div>
+      </div>
+      
+      <button class="button1">検索</button>
+        
+</x-app-layout>
